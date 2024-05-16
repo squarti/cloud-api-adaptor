67c3e3c5b0e56bc4d3308c0523f615c50e20a672
Silenio Quarti <silenio_quarti@ca.ibm.com>
2024-05-16 17:50:35 -0400

    Revert "build power"
    This reverts commit c477f3c5db81f11d4c93d62dcddcef0a4c5c8dca

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -3,7 +3,6 @@
 GO_VERSION="1.21.9"
 RUST_VERSION="1.72.0"
 SKOPEO_VERSION="1.5.0"
-YQ_VERSION="v4.35.1"
 
 # Install dependencies
 yum install -y curl protobuf-compiler libseccomp-devel openssl openssl-devel perl skopeo-${SKOPEO_VERSION}
@@ -16,6 +15,3 @@ rm -f go${GO_VERSION}.linux-ppc64le.tar.gz
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${RUST_VERSION}
 rustup target add powerpc64le-unknown-linux-gnu
-
-curl https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_ppc64le -o /usr/local/bin/yq
-chmod a+x /usr/local/bin/yq
